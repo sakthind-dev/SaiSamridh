594d1e6f1f32909796b96f7202dc0e28bc7ce664
sakthind-dev <dsn2603@rediffmail.com>
2026-06-06 11:23:03 +0530

    Update paths in make.bat for OpenCppCoverage

--- C/AI_Test_Setup/make.bat
+++ C/AI_Test_Setup/make.bat
@@ -13,15 +13,15 @@ gcov test_math1.c
 gcov -o . math1.c
 
 ::OpenCppCoverage.exe --sources . -- test_math1.exe
-::OpenCppCoverage.exe --sources C:\Users\saijo\OneDrive\Desktop\Learnings\SaiSamridh\C -- test_math1.exe
-::OpenCppCoverage.exe --sources C:\Users\saijo\OneDrive\Desktop\Learnings\SaiSamridh\C --modules C:\Users\saijo\OneDrive\Desktop\Learnings\SaiSamridh\C -- test_math1.exe
+::OpenCppCoverage.exe --sources Learnings\SaiSamridh\C -- test_math1.exe
+::OpenCppCoverage.exe --sources earnings\SaiSamridh\C --modules SaiSamridh\C -- test_math1.exe
 ::OpenCppCoverage.exe --sources . -- test_math1.exe
 
 gcc -g -fprofile-arcs -ftest-coverage test_math1.c math1.c unity.c -o test_math1.exe
 .\test_math1.exe
-OpenCppCoverage.exe --sources C:\Users\saijo\OneDrive\Desktop\Learnings\SaiSamridh\C --modules C:\Users\saijo\OneDrive\Desktop\Learnings\SaiSamridh\C -- test_math1.exe
+OpenCppCoverage.exe --sources Learnings\SaiSamridh\C --modules Learnings\SaiSamridh\C -- test_math1.exe
 
 
-:: C:\Users\saijo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\site-packages\gcovr\coverage.py -r . --html --html-details -o coverage.html
+:: Python312\site-packages\gcovr\coverage.py -r . --html --html-details -o coverage.html
 
 python -m gcovr =v -r . --html --html-details -o coverage.html
